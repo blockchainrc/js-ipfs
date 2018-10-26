7f6495d1df9d4fcf00d8a5e9b61a481f44d7fb27
David Dias <daviddias.p@gmail.com>
2018-10-26 15:07:46 +0200

    feat: update CLI to use latest Files API

--- src/cli/bin.js
+++ src/cli/bin.js
@@ -11,7 +11,7 @@ const print = utils.print
 const mfs = require('ipfs-mfs/cli')
 const debug = require('debug')('ipfs:cli')
 
-const pkg = readPkgUp.sync({cwd: __dirname}).pkg
+const pkg = readPkgUp.sync({ cwd: __dirname }).pkg
 updateNotifier({
   pkg,
   updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week
@@ -74,19 +74,8 @@ if (args[0] === 'daemon' || args[0] === 'init') {
         throw err
       }
 
-      // add mfs commands
+      // add MFS (Files API) commands
       mfs(cli)
-
-      // NOTE: This creates an alias of
-      // `jsipfs files {add, get, cat}` to `jsipfs {add, get, cat}`.
-      // This will stay until https://github.com/ipfs/specs/issues/98 is resolved.
-      const addCmd = require('./commands/files/add')
-      const catCmd = require('./commands/files/cat')
-      const getCmd = require('./commands/files/get')
-      const aliases = [addCmd, catCmd, getCmd]
-      aliases.forEach((alias) => {
-        cli.command(alias)
-      })
 
       cli
         .commandDir('commands')
@@ -118,7 +107,7 @@ if (args[0] === 'daemon' || args[0] === 'init') {
           exitCode = 1
         })
         .then(() => cleanup())
-        .catch(() => {})
+        .catch((e) => {})
         .then(() => {
           if (exitCode !== 0) {
             process.exit(exitCode)
